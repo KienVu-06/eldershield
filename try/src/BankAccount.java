9d9a4ce0adf937128b814e0f3ae067e5afa2568c
Kien Vu <vunguyentrungkien.10ly.k32@gmail.com>
2026-03-20 22:28:04 +0700

    feat: Deposit and withdraw and pay bills

--- try/src/BankAccount.java
+++ try/src/BankAccount.java
@@ -1,5 +1,7 @@
 package main;
 
+import model.BankAccount;
+
 public class BankAccount {
 	private String accountNumber;
 	private String owner;
@@ -26,5 +28,46 @@ public class BankAccount {
 	}
 	public double getBalance() {
 		return balance;
+	}
+	public void deposit(double amount) {
+		if(amount <= 0) {
+			System.out.println("the deposit operation is fail");
+		}
+		else {
+			balance += amount;
+		}
+	}
+	public void withdraw(double amount) {
+		if(amount <= 0 || balance - amount < THE_MINIMUM_BALANCE) {
+			System.out.println("The withdraw operation is faill");
+		}
+		else {
+			balance -= amount;
+		}
+	}
+	public void transferMoney(double amount, BankAccount receiver) {
+		if(receiver == null) {
+			System.out.println("The receiver is not exist. The transfer operation is fail");
+			return ;
+		}
+		if (amount <= 0 || balance - amount - THE_FEE*amount < THE_MINIMUM_BALANCE ) {
+			System.out.println("The transfer operation is fail");
+		}
+		else {
+			balance -= (amount + THE_FEE*amount);
+			receiver.balance += amount;
+			System.out.println("The account number " + accountNumber + " of the owner " + owner + " transfer " + amount + " for the account number " + receiver.accountNumber + " of the owner " + receiver.owner);
+			System.out.println("The current balance of the account number " + accountNumber + " of the owner " + owner + " is " + balance);
+			System.out.println("The current balance of the account number " + receiver.accountNumber + " of the owner " + receiver.owner + " is " + receiver.balance);
+		}
+	}
+	public void payBill(double amount, String nameBill) {
+		if(nameBill.trim().isEmpty() || amount <= 0 || balance - amount < THE_MINIMUM_BALANCE) {
+			System.out.println("The pay bills operation is fail");
+		}
+		else {
+			balance -= amount;
+			System.out.println("The bill " + nameBill + " is paid successfully." + " The current balance is " + balance);
+		}
 	}
 }
