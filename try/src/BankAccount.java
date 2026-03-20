204870be5943c735f96ca6908e08e27e419cec9f
Kien Vu <vunguyentrungkien.10ly.k32@gmail.com>
2026-03-20 22:24:21 +0700

    feat: Add Constructors and Getters/Setters

--- try/src/BankAccount.java
+++ try/src/BankAccount.java
@@ -6,4 +6,25 @@ public class BankAccount {
 	private double balance;
 	public static final double THE_MINIMUM_BALANCE = 50000;
 	public static final double THE_FEE = 0.02;
+	public BankAccount(String accountNumber, String owner, double balance) {
+		if(owner.trim().isEmpty()) {
+			throw new IllegalArgumentException("owner is not valid");
+		}
+		if(balance < THE_MINIMUM_BALANCE) {
+			throw new IllegalArgumentException("balance is not valid");
+		}
+		this.accountNumber = accountNumber;
+		this.owner = owner;
+		this.balance = balance;
+		System.out.println("The new account is created successfully. The account number is " + accountNumber + " of the owner " + owner + " with the initial balance " + balance);
+	}
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public String getOwner() {
+		return owner;
+	}
+	public double getBalance() {
+		return balance;
+	}
 }
